e1c396b5bea7018f72298a15d992906173cc8c3f
digschauhan <digs.chauhan@gmail.com>
2016-03-01 23:53:14 -0700

    ccc

--- src/com/http/serverclient/MyHttpServer.java
+++ src/com/http/serverclient/MyHttpServer.java
@@ -3,12 +3,15 @@ package com.http.serverclient;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URL;
 
 public class MyHttpServer {
 
-	public static void main(String[] args) {
+	
+	public void startServer(){
 		try {
 			ServerSocket serverSocket = new ServerSocket(4567);
 			System.out.println("HTTP Server (only POST implemented) is ready and is listening on Port Number 4567 \n");
@@ -23,7 +26,7 @@ public class MyHttpServer {
 				String response = "HTTP/1.1 200 OK\n\r";
 				response = response + "Date: Fri, 04 May 2001 20:08:11 GMT\n\r";
 				response = response + "1";
-				req.append(response);
+				//req.append(response);
 				int i=1;
 				
 				/*while ((temp = in.readLine()) != null){
@@ -46,13 +49,50 @@ public class MyHttpServer {
 				out.flush();
 				in.close();
 				out.close();
+				serverSocket.close();
 				
+				sendHookup(req.toString());
 			}
 		} catch (Exception e) {
 			System.out.println("ERROR: " + e.getMessage());
 			System.exit(1);
 		}
+		
+	}
+	
+	public void sendHookup(String response){
+		try {
+			URL url = new URL("http://localhost:8080/github-webhook/");
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setDoOutput(true);
+			con.setDoInput(true);
+			con.setRequestMethod("POST");
+			con.setUseCaches(false);
+			//String test = "<name>Hello</name>";
+			byte[] bytes = response.getBytes();
+			con.setRequestProperty("Content-length", String.valueOf(bytes.length));
+			con.setRequestProperty("Content-type", "text/html");
+			OutputStream out = con.getOutputStream();
+			out.write(bytes);
+			out.flush();
+			out.close();
+			/*BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String temp;
+			while ((temp = in.readLine()) != null){
+				System.out.println(temp);
+			}
+			out.close();
+			in.close();*/
+			con.disconnect();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
 
+	public static void main(String[] args) {
+		MyHttpServer server = new MyHttpServer();
+		server.startServer();
 	}
 
 }
