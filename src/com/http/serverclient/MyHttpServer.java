f6b0020b110738294a28ab49fa39cfa431d91c3e
digschauhan <digs.chauhan@gmail.com>
2016-03-01 21:51:55 -0700

    updated

--- src/com/http/serverclient/MyHttpServer.java
+++ src/com/http/serverclient/MyHttpServer.java
@@ -11,22 +11,24 @@ public class MyHttpServer {
 	public static void main(String[] args) {
 		try {
 			ServerSocket serverSocket = new ServerSocket(4567);
-			System.out.println("HTTP Server (only POST implemented) is ready and is listening on Port Number 12001 \n");
+			System.out.println("HTTP Server (only POST implemented) is ready and is listening on Port Number 4567 \n");
 			while (true) {
 				Socket clientSocket = serverSocket.accept();
 				System.out.println(clientSocket.getInetAddress().toString() + " " + clientSocket.getPort());
 				BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 				OutputStream out = clientSocket.getOutputStream();
+				StringBuffer req=new StringBuffer();
 				String temp;
 				while ((temp = in.readLine()) != null){
-					System.out.println(temp);
+					req.append(temp);
+					//System.out.println(temp);
 				}
+				System.out.println(req.toString());
+				
 				String response = "HTTP/1.1 200 OK\n\r";
 				response = response + "Date: Fri, 04 May 2001 20:08:11 GMT\n\r";
-				response = response + "Server: Sanjits Server\n\r";
-				response = response + "Connection: close\n\r";
 				response = response + "1";
-				byte[] bytes = response.getBytes();
+				byte[] bytes = req.toString().getBytes();
 				out.write(bytes);
 				out.flush();
 				in.close();
